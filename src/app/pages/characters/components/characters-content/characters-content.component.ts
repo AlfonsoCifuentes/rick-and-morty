c994ae45aed73f3d8853d014ab73ce801bf890ae
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-12-29 03:16:09 +0100

    Añadidos botones de pasar página y cambios CSS

--- src/app/pages/characters/components/characters-content/characters-content.component.ts
+++ src/app/pages/characters/components/characters-content/characters-content.component.ts
@@ -9,14 +9,31 @@ import { ICharacterList } from '../../models/icharacterList';
 })
 export class CharactersContentComponent implements OnInit {
 
-  private URL: string="https://rickandmortyapi.com/api/character";
+  private URL: string="https://rickandmortyapi.com/api/character/?page=1";
   public characterList: ICharacterList[] = [];
+
+
+  public nextURL: string="";
+  public prevURL: string="";
+
+  
 
   constructor(private HttpRequestService: HttpRequestService) { }
 
   ngOnInit(): void {
     this.recoverCharacters();
   }
+
+  nextUrlClick(): void {
+    this.URL = this.nextURL
+    this.recoverCharacters(); 
+  }
+
+  prevUrlClick(): void {
+    this.URL = this.prevURL
+    this.recoverCharacters();
+  }
+
 
   public recoverCharacters() {
     this.HttpRequestService.getData(this.URL).subscribe((data: any)=> {
@@ -26,7 +43,12 @@ export class CharactersContentComponent implements OnInit {
         image,
       }));
       this.characterList = formattedResults;
+
+      this.nextURL = data.info.next;
+      this.prevURL = data.info.prev;
+
     })
   }
+
 
 }
--- src/app/pages/locations/components/locations-content/locations-content.component.ts
+++ src/app/pages/locations/components/locations-content/locations-content.component.ts
@@ -9,14 +9,28 @@ import { ILocation } from '../../models/ilocation';
 })
 export class LocationsContentComponent implements OnInit {
 
-  private URL: string="https://rickandmortyapi.com/api/location";
+  private URL: string="https://rickandmortyapi.com/api/location?page=1";
   public locations: ILocation[] = [];
+
+  public nextURL: string="";
+  public prevURL: string="";
 
   constructor(private HttpRequestService: HttpRequestService) { }
 
   ngOnInit(): void {
     this.recoverLocations();
   }
+
+  nextUrlClick(): void {
+    this.URL = this.nextURL
+    this.recoverLocations(); 
+  }
+
+  prevUrlClick(): void {
+    this.URL = this.prevURL
+    this.recoverLocations();
+  }
+
 
   public recoverLocations() {
     this.HttpRequestService.getData(this.URL).subscribe((data: any)=> {
@@ -28,6 +42,9 @@ export class LocationsContentComponent implements OnInit {
 
       }))
       this.locations = formattedResults;
+      
+      this.nextURL = data.info.next;
+      this.prevURL = data.info.prev;
     })
   }
 }
